cdabb016aedbca14794bb2d01530025bc3d911cc
Mauricio Wurth <mauri.wurth@gmail.com>
2022-08-28 15:04:21 -0300

    Simulador

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 let invitados = 0;
-let edadInvitados;
 
-while (invitados >= 0) {
+while (invitados >= 0){
   if (prompt('¿Deseas agregar invitados?').toLowerCase() === 'si') {
 
-    let edadIngresada = prompt('Ingrese la edad');
+    let edadIngresada = parseInt(prompt('Ingrese la edad'));
 
-    if ((edadIngresada >= 18) && (edadIngresada <= 100)) {
-      // invitados == invitados + 1;
+    if (checkAge(edadIngresada)) {
       invitados++;
       alert('Invitación creada correctamente.')
-
     } else {
       alert('Tu edad no está permitida.')
     }
 
-  } else {
+   } else {
     break;
   }
+    
 }
 
 alert('Tienes ' + invitados + ' invitados.');
+
+function checkAge(age){
+  if ((age >= 18) && (age <= 100)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
