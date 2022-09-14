5e215e950d49a4a7fcf9480102f799151d51931f
Mauricio Wurth <mauri.wurth@gmail.com>
2022-09-14 19:48:36 -0300

    interfaz y eventos

--- index.js
+++ index.js
@@ -1,47 +1,42 @@
 let invitados = [];
 
-while (invitados.length >= 0){
-  if (prompt('¿Deseas agregar invitados?').toLowerCase() === 'si') {
-
-    let nombreIngresada = prompt('Ingrese el nombre:');
-    let edadIngresada = parseInt(prompt('Ingrese la edad:'));
-    let generoIngresada = prompt('Ingrese el género:');
-    let colaboracionIngresada = parseInt(prompt('Ingrese la colaboración:'));
-
-
-    if (checkAge(edadIngresada)) {
-      invitados.push(new Invitado(nombreIngresada,edadIngresada,generoIngresada,colaboracionIngresada))
-      alert('Invitación creada correctamente.')
-    } else {
-      alert('Tu edad no está permitida.')
-    }
-
-   } else {
-    break;
+let form = document.getElementById("invitadoData");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let nombre = document.getElementById('nombre').value;
+  let edad = parseInt(document.getElementById('edad').value);
+  let genero = document.getElementById('genero');
+  if (genero.value === 'm') {
+    genero = 'Masculino'
+  } else if (genero.value === 'f') {
+    genero = 'Femenino'
+  } else {
+    genero = 'No está seguro'
   }
-    
-}
+  let colaboracion = parseInt(document.getElementById('colaboracion').value);
+  if (checkAge(edad)) {
+    invitados.push(new Invitado(nombre, edad, genero, colaboracion))
+    drawTable()
+    alert('Invitación creada correctamente.')
+  } else {
+    alert('Tu edad no está permitida.')
+  }
+})
 
+// let termino;
+// if (invitados.length === 1) {
+//   termino = 'mesa'
+// } else {
+//   termino = 'mesas'
+// }
+// console.table(invitados);
+// console.log(`Se van a necesitar en total ${Math.ceil(invitados.length / 6)} ${termino}.`);
+// const totalColaboracion = invitados.reduce(
+//   (previousValue, currentValue) => previousValue + currentValue.colaboracion,
+//   0
+// );
 
-
-let termino;
-if (invitados.length === 1){
-  termino = 'mesa'
-} else {
-  termino = 'mesas'
-}
-console.table(invitados);
-console.log(`Se van a necesitar en total ${Math.ceil(invitados.length/6)} ${termino}.`);
-const totalColaboracion = invitados.reduce(
-  (previousValue, currentValue) => previousValue + currentValue.colaboracion,
-  0
-);
-
-
-
-console.log(`total colaboración ${totalColaboracion} `);
-
-function checkAge(age){
+function checkAge(age) {
   if ((age >= 18) && (age <= 100)) {
     return true;
   } else {
@@ -49,9 +44,30 @@ function checkAge(age){
   }
 }
 
-function Invitado(nombre, edad, genero, colaboracion){
+function Invitado(nombre, edad, genero, colaboracion) {
   this.nombre = nombre;
   this.edad = edad;
   this.genero = genero;
   this.colaboracion = colaboracion;
+}
+
+function drawTable(){
+  // Dibujando en el HTML
+  const table = document.querySelector('table');
+  let data = `<tr>
+<th>Nombre</th>
+<th>Edad</th>
+<th>Género</th>
+<th>Colaboración</th>
+</tr>`;
+
+  for (let invitado of invitados) {
+    data += `<tr>
+  <td>${invitado.nombre}</td>
+  <td>${invitado.edad}</td>
+  <td>${invitado.genero}</td>
+  <td>${invitado.colaboracion}</td>
+</tr>`
+  }
+  table.innerHTML = data;
 }
